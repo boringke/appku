44e8dfaffd106d1b0869e2dace499b94e9501b53
Zulkifli Mohamed <mr.putera@gmail.com>
2021-05-25 22:53:08 +0800

    update TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: [
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.vue',
+    // './resources/**/*.blade.php',
+    // './resources/**/*.js',
+    // './resources/**/*.vue',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
